90f80718db485ce33b9759aa8c8a63fa65a9206e
mousomray <mousomray02@gmail.com>
2024-08-26 18:24:04 +0530

    I make some changes

--- endpoint_cart_filtersearch/src/Endpoints/endpoint.js
+++ endpoint_cart_filtersearch/src/Endpoints/endpoint.js
@@ -1,14 +1,9 @@
 export const endpoints = {
-    // auth:{
-    //     register:"register",
-    //     login:"login"
-    // },
     cms: {
         productlist: "products"
     }
 }
 
-export const successNotificationEndPoints = [
-    // endpoints.auth.register, //If Register or Login Present
-    endpoints.cms.productlist
+export const myendpoints = [
+    endpoints.cms.productlist // This is array index number 0 
 ]
--- endpoint_cart_filtersearch/src/Pages/allreducers.js
+++ endpoint_cart_filtersearch/src/Pages/allreducers.js
@@ -1,12 +1,13 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit"; //createAsyncThunk handle asynconomous function 
 import axiosInstance from "../api/api"
-import { endpoints } from "../Endpoints/endpoint";
+// import { endpoints } from "../Endpoints/endpoint"; You can direct import endpoint like this 
+import { myendpoints } from "../Endpoints/endpoint"; // You can also import endpoint like this 
 
 
 // Call Api for Product
 export const product = createAsyncThunk("product", async (_, { rejectWithValue }) => {
     try {
-        const apiurl = `${endpoints.cms.productlist}`
+        const apiurl = myendpoints[0]; //[0] means First array element 
         const response = await axiosInstance.get(apiurl);
         console.log("Fetching Product List", response);
         return response?.data
@@ -19,7 +20,7 @@ export const product = createAsyncThunk("product", async (_, { rejectWithValue }
 // Call Api for Single Product
 export const singleproduct = createAsyncThunk("singleproduct", async (id, { rejectWithValue }) => {
     try {
-        const apiurl = `${endpoints.cms.productlist}/${id}`
+        const apiurl = `${myendpoints[0]}/${id}`
         const response = await axiosInstance.get(apiurl);
         console.log("Fetching Single Product", response);
         return response?.data
